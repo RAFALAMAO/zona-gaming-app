89ce97be918480a05263d07526f77c6350d2185c
Aaron JT <aaron.juarez@tec.mx>
2025-09-08 01:17:29 -0600

    feat: :sparkles: se agrega la animación de numeros en el hero

--- src/components/Home/HeroBanner/HeroBanner.tsx
+++ src/components/Home/HeroBanner/HeroBanner.tsx
@@ -1,3 +1,5 @@
+import AnimatedNumbers from 'react-animated-numbers';
+
 // ** Styles
 import styles from './HeroBanner.module.css';
 
@@ -18,15 +20,45 @@ export default function HeroBanner() {
         <p className="lead mt-3">Venta, compra y cambio de tecnología a los mejores precios.</p>
         <div className="row mt-4 ms-5 me-5">
           <div className="col-12 col-sm-4">
-            <h4 className="fw-bold">{countAvailables}+</h4>
+            <h4 className="fw-bold">
+              <AnimatedNumbers
+                useThousandsSeparator
+                transitions={(index) => ({
+                  type: 'spring',
+                  duration: index + 5,
+                })}
+                animateToNumber={countAvailables}
+              />
+              +
+            </h4>
             <p className={styles['sub-text']}>Productos</p>
           </div>
           <div className="col col-sm-4">
-            <h4 className="fw-bold">{categories.length}+</h4>
+            <h4 className="fw-bold">
+              <AnimatedNumbers
+                useThousandsSeparator
+                transitions={(index) => ({
+                  type: 'spring',
+                  duration: index + 6,
+                })}
+                animateToNumber={categories.length}
+              />
+              +
+            </h4>
             <p className={styles['sub-text']}>Categorias</p>
           </div>
           <div className="col col-sm-4">
-            <h4 className="fw-bold">0+</h4>
+            <h4 className="fw-bold">
+              <AnimatedNumbers
+                useThousandsSeparator
+                transitions={(index) => ({
+                  type: 'spring',
+                  duration: index + 6,
+                })}
+                animateToNumber={10}
+              />
+              +
+            </h4>
             <p className={styles['sub-text']}>Marcas</p>
           </div>
         </div>
